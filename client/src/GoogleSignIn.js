517d45828366623c66a8af8c0294b6d36865e9e3
Hannah Nguyen <hnguyen0@usc.edu>
2021-02-25 00:24:15 -0800

    google authentication and routes ('with' Terry ( :p ))

---
+++ client/src/GoogleSignIn.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
+import { Redirect } from "react-router-dom";
+
+export default function GoogleSignIn() {
+    const [isAuthenticated, setAuthenticated] = useState(false);
+
+    const handleLogin = async googleData => {
+        const res = await fetch("api/login/auth/google", {
+            method: "POST",
+            body: JSON.stringify({
+                token: googleData.tokenId
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        const data = await res.json();
+
+        if (res.status === 201) {
+            setAuthenticated(true);
+        }
+
+        console.log(data);
+    }
+
+    return (
+        <>
+            <GoogleLogin
+                clientId={"903480499371-fqef1gdanvccql6q51hgffglp7i800le.apps.googleusercontent.com"}
+                buttonText="Log in with Google"
+                onSuccess={handleLogin}
+                onFailure={handleLogin}
+            />
+            { isAuthenticated === true ? <Redirect to="/dashboard" /> : null}
+        </>
+
+    );
+}
