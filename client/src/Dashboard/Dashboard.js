a16ac38f3e868a0d297e2a0dbdb0b74f9a436caa
eberge <eberge@eberges-mbp.myfiosgateway.com>
2021-03-11 16:56:25 -0500

    add partially works

--- client/src/Dashboard/Dashboard.js
+++ client/src/Dashboard/Dashboard.js
@@ -1,15 +1,94 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import StudentGrid from "./StudentGrid.js";
-
-
+import {InputGroup, FormControl, Button} from 'react-bootstrap'
+import { DashboardContext } from "../contexts.js";
+import {
+  GridContextProvider,
+  GridDropZone,
+  GridItem,
+  swap
+} from "react-grid-dnd";
 
 
 export default function Dashboard () {
+
+
+    const [students, setStudents] = useState(["First, Last 1", "First, Last 2", "First, Last 3", "First, Last 4", "First, Last 5", 
+    "First, Last 6", "First, Last 7", "First, Last 8", "First, Last 9", "First, Last 10",  "First, Last 11", "First, Last 12", "First, Last 13",
+    "First, Last 14", "First, Last 15", "First, Last 16", "First, Last 17", "First, Last 18", "First, Last 19", "First, Last 20", "First, Last 21", 
+    "First, Last 22", "First, Last 23", "First, Last 24", "First, Last 25", "First, Last 26", "First, Last 27", "First, Last 28", "First, Last 29",
+    "First, Last 30", "First, Last 31", "First, Last 32", "First, Last 33", "First, Last 34", "First, Last 35", "First, Last 36"]); 
+    var s = students;
+
+    const [studentName, setStudentName] = useState("");
+    const [size, setSize] = useState((students.length/6)*100)
+    // target id will only be set if dragging from one dropzone to another.
+    function onChange(sourceId, sourceIndex, targetIndex, targetId) {
+      const nextState = swap(students, sourceIndex, targetIndex);
+      setStudents(nextState);
+    }
+
+    function addStudent(event){
+        event.preventDefault();
+        students.push(studentName);
+        setStudents(students);
+        s = students;
+        if(students.length%6 == 1){
+          setSize(size+100);
+        }
+     
+    }
+    
+    function handleNameChange(event) {
+      setStudentName(event.target.value);
+    }
+   
     return (
-      <div className = "grid-container">
-        <StudentGrid></StudentGrid>
+    
+        <div className = "d-flex"> 
+        <div className="add-button-container"> 
+        <form onSubmit = {addStudent} >
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Student Name"
+              aria-label="Student Name"
+              aria-describedby="basic-addon2"
+              value = {studentName}
+              onChange={handleNameChange}
+            />
+            <InputGroup.Append>
+              <Button className = "add-button" type="submit">+</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </form>
+        
+        </div>
+        <div className = "grid-container">
+        <GridContextProvider onChange={onChange}>
+          <GridDropZone
+            id="items"
+            className="dropzone"
+            boxesPerRow={6}
+            rowHeight={100}
+            style = {{height: size}}
+          >
+          {s && s.length ? (
+            s.map(item => (
+              <GridItem key={item}>
+                  <div className = "grid-item">
+                    <div className = "tile">
+                        {item}
+                    </div>
+                  </div>
+                
+              </GridItem>
+            )) ) : <></>}
+          </GridDropZone>
+        </GridContextProvider>
+        </div>
       </div>
-      
+
+
     );
     
   }
--- client/src/Dashboard/StudentGrid.js
+++ client/src/Dashboard/StudentGrid.js
@@ -1,25 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
 import {
     GridContextProvider,
     GridDropZone,
     GridItem,
     swap
 } from "react-grid-dnd";
+import { DashboardContext } from '../contexts';
 
-export default function Test(){
-
- const [items, setItems] = useState(["First, Last 1", "First, Last 2", "First, Last 3", "First, Last 4", "First, Last 5", 
- "First, Last 6", "First, Last 7", "First, Last 8", "First, Last 9", "First, Last 10",  "First, Last 11", "First, Last 12", "First, Last 13",
- "First, Last 14", "First, Last 15", "First, Last 16", "First, Last 17", "First, Last 18", "First, Last 19", "First, Last 20", "First, Last 21", 
- "First, Last 22", "First, Last 23", "First, Last 24", "First, Last 25", "First, Last 26", "First, Last 27", "First, Last 28", "First, Last 29",
- "First, Last 30", "First, Last 31", "First, Last 32", "First, Last 33", "First, Last 34", "First, Last 35", "First, Last 36"]); 
- const [size, setSize] = useState((items.length/6)*100)
+export default function StudentGrid({students, setStudents}){
+ 
+ 
+ const [size, setSize] = useState((students.length/6)*100)
   // target id will only be set if dragging from one dropzone to another.
   function onChange(sourceId, sourceIndex, targetIndex, targetId) {
-    const nextState = swap(items, sourceIndex, targetIndex);
-    setItems(nextState);
+    const nextState = swap(students, sourceIndex, targetIndex);
+    setStudents(nextState);
   }
-  
+
+
   return (
     <GridContextProvider onChange={onChange}>
       <GridDropZone
@@ -29,7 +27,8 @@ export default function Test(){
         rowHeight={100}
         style = {{height: size}}
       >
-        {items.map(item => (
+      {students && students.length ? (
+        students.map(item => (
           <GridItem key={item}>
               <div className = "grid-item">
                 <div className = "tile">
@@ -38,7 +37,7 @@ export default function Test(){
               </div>
             
           </GridItem>
-        ))}
+        )) ) : <></>}
       </GridDropZone>
     </GridContextProvider>
   )
