5c8e7ce0178e33390624dc1dababad0830628c28
Terry <tcwang@usc.edu>
2021-03-13 17:16:27 -0600

    Added jwt verification sync cause it doesnt matter

--- api/routes/period.js
+++ api/routes/period.js
@@ -6,10 +6,22 @@ var morgan  = require('morgan');
 const mongoose = require('mongoose');
 const period = require('../models/period')
 
-//Set up default mongoose connection
-module.exports = router;
+//Static cause no performance benefit to not being Static
+function verify(callback){
+  try{
+    const decoded = jwt.verify(req.cookies.c_user, process.env.JWT_SECRET_KEY);
+    callback();
+  }
+  catch(err){
+    console.log(err);
+  }
+}
 
-router.get("/students/update", function(req, res) {
+//async verify test
+
+router.get("/students/update", async function(req, res) {
+  try{
+    const decoded = jwt.verify(req.cookies.c_user, process.env.JWT_SECRET_KEY);
     period.findByIdAndUpdate(req.body.period_id, req.body.period, function(err, result) {
       if (err) {
         res.send(err);
@@ -17,10 +29,18 @@ router.get("/students/update", function(req, res) {
         res.send(result);
       }
     });
+  }
+  catch(err){
+    console.log(err);
+  }
+
+
 });
 
 //Update Query
 router.get("/students/update/:period_id", function(req, res) {
+  try{
+    const decoded = jwt.verify(req.cookies.c_user, process.env.JWT_SECRET_KEY);
     period.findByIdAndUpdate(req.params['period_id'], req.body, function(err, result) {
       if (err) {
         res.send(err);
@@ -28,33 +48,46 @@ router.get("/students/update/:period_id", function(req, res) {
         res.send(result);
       }
     });
+  }
+  catch(err){
+    console.log(err);
+  }
 });
 
 router.get("/students/create", function(req,res) {
-  const per = new period({
-    students:[
-      {
-        student_id: 0,
-        name: "Meow"
-      }
-    ]
-  });
-  // const per = new period(req.body);
-  per.save()
-    .then((result) => {
-      res.send(result)
-    })
-    .catch((err) =>{
+    try{
+      const decoded = jwt.verify(req.cookies.c_user, process.env.JWT_SECRET_KEY);
+      const per = new period(req.body);
+      // const per = new period(req.body);
+      per.save()
+        .then((result) => {
+          res.send(result)
+        })
+        .catch((err) =>{
+          console.log(err);
+        });
+    }
+    catch(err){
       console.log(err);
-    });
+    }
+
 });
 
 router.get("/students/seating/:period_id", function(req,res) {
-  period.findById(req.params['period_id'])
-    .then((results) =>{
-      res.send(results);
-    })
-    .catch((err) =>{
+
+    try{
+      const decoded = jwt.verify(req.cookies.c_user, process.env.JWT_SECRET_KEY);
+      period.findById(req.params['period_id'])
+        .then((results) =>{
+          res.send(results);
+        })
+        .catch((err) =>{
+          console.log(err);
+        });
+    }
+    catch(err){
       console.log(err);
-    })
+    }
 });
+
+module.exports = router;
