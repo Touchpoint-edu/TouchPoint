33cf3c81d814fe617ffebe50a7b9ed7458546bd5
Hannah Nguyen <hnguyen0@usc.edu>
2021-04-11 23:00:51 -0700

    post instead of get to add behavior

--- client/src/api/behavior.js
+++ client/src/api/behavior.js
@@ -1,6 +1,6 @@
 export async function addBehavior(studentId, behaviorName) {
     const res = await fetch("behavior/add/" + studentId, {
-        method: "GET",
+        method: "POST",
         body: JSON.stringify({
             behavior_name: behaviorName
           })
