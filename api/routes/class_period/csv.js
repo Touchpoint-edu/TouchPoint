f9bc279643cecbaa85558c6ef8af7b34416f706e
Terry <tcwang@usc.edu>
2021-05-02 18:59:39 -0500

    removed code in backend

--- api/routes/class_period/csv.js
+++ api/routes/class_period/csv.js
@@ -12,8 +12,6 @@ const { CSV_FORMAT_ERROR_MSG, SERVER_ERROR_MSG } = require('../../constants/erro
 var router = express.Router();
 const filesMulter = multer({ dest: 'csv/' });
 
-const DEFAULT_COL_SIZE = 6;
-
 /**
  * Parse csv file to save all students into the database and create a period
  * Return the period with students array in the response body
@@ -21,41 +19,33 @@ const DEFAULT_COL_SIZE = 6;
 router.post("/upload", filesMulter.single('file'), async (req, res) => {
     try {
         const userPayload = verify.verify(req.cookies.c_user, process.env.JWT_SECRET_KEY);
-        let currRow = 0, currCol = 0;
+
         let students = [];
         const parseFileOptions = {
             headers: [undefined, 'name', 'email', undefined],
             skipLines: 6
         }
+
         csv.parseFile(req.file.path, parseFileOptions)
             .on("error", () => {
                 fs.unlinkSync(req.file.path);   // remove temp file
                 error.sendError(res, 400, CSV_FORMAT_ERROR_MSG)
             })
             .on("data", data => {
-                data._id = new ObjectId();
-                data.row = currRow;
-                data.col = currCol;
-                currCol++;
-                if (currCol >= 6) {
-                    currRow++;
-                    currCol = 0;
-                }
+                data._id = new ObjectId()
                 students.push(data);
             })
             .on("end", () => {
                 fs.unlinkSync(req.file.path);   // remove temp file
 
                 // create and save period to user
-                createPeriod(students, userPayload.sub, currRow, parseInt(req.body.period)).then((result, err) => {
-                    if (err) {
-                        error.sendError(res, 500, SERVER_ERROR_MSG);
-                    }
-                    else {
-                        res.sendStatus(200);
-                    }
+                const period = createPeriod(students);
+                savePeriodToUser(period._id, userPayload.sub);
+
+                res.status(200);
+                res.json({
+                    period: period
                 });
-                // savePeriodToUser(period._id, userPayload.sub);
             })
     } catch (err) {
         console.log(err);
@@ -66,15 +56,13 @@ router.post("/upload", filesMulter.single('file'), async (req, res) => {
 /**
  * create a csv file of students and their behaviors with counts of each behavior
  */
- router.post("/download", (req, res) => {
+ router.get("/download", (req, res) => {
     try {
         const userPayload = verify.verify(req.cookies.c_user, process.env.JWT_SECRET_KEY);
-        res.setHeader("Content-Type", "text/csv");
-        res.setHeader("Content-Disposition", "attachment; test.csv");
-        res.sendFile(__dirname + "/test.csv");
+
         // download
 
-        //res.sendStatus(200);
+        res.sendStatus(200);
 
     } catch (err) {
         console.log(err);
@@ -89,25 +77,20 @@ router.post("/upload", filesMulter.single('file'), async (req, res) => {
  * @param {*} students : array of students
  * @returns the period created in the db
  */
- const createPeriod = (students, id, rowNum, periodNum) => {
+ const createPeriod = (students) => {
     const period = {
-        $set: {
-            rows: rowNum+1,
-            columns: DEFAULT_COL_SIZE,
-            user_id: new ObjectId(id),
-            students: students,
-            periodNum: periodNum
-        }
+        columns: 6,
+        students: students
     }
-    const query = { 
-        user_id: new ObjectId(id), 
-        periodNum: 
-        periodNum
-    }
-    const options = {
-        upsert: true
-    }
-    return mongo.update("periods", query, period, options);
+
+    mongo.insertOne("periods", period)
+        .then(data => {
+            if (!data) {
+                error.sendError(res, 500, SERVER_ERROR_MSG);
+            }
+        });
+
+    return period
 }
 
 /**
