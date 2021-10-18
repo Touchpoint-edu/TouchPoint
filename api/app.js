a634036920bbd2109ed130a2e2aa65fb0801e24e
Amanda Wang <wangyira@usc.edu>
2021-10-17 19:16:03 -0700

    set environment variable to development locally

--- api/app.js
+++ api/app.js
@@ -34,9 +34,43 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 //PLAY AROUND HERE
 dotenv.config();
 
-console.log(process.env.MONGO_DB_URI);
+console.log("process mongo db uri", process.env.MONGO_DB_URI);
 
-if (process.env.location === "production"){
+console.log("TOUCHPOINT_ENVIRONMENT", process.env.TOUCHPOINT_ENVIRONMENT)
+
+if(process.env.TOUCHPOINT_ENVIRONMENT === "development"){
+  console.log("in local development")
+  const jwtLoginSecret = process.env.JWT_LOGIN_SECRET;
+  console.log("jwtloginsecret",jwtLoginSecret)
+  const jwtVerifySecret = process.env.JWT_VERIFY_SECRET;
+  const emailObj = {
+      email: process.env.EMAIL_USERNAME,
+      password: process.env.EMAIL_PASSWORD
+  }
+  
+  app.use(function(req, res, next) {
+      req.jwtLoginSecret = jwtLoginSecret;
+      req.jwtVerifySecret = jwtVerifySecret;
+      req.emailCredentials = emailObj;
+      next();
+  });
+  
+  
+  mongo.connect(process.env.MONGO_DB_URI, async function(err) {
+          //Add routes here
+          if (err) {
+            console.log("throwing error inside mongo connect");
+            throw err;
+          }
+          app.use("/test", testRouter);
+          app.use("/api/auth", authRouter);
+          app.use("/api/period", classPeriodRouter);
+          app.use("/api/behavior", behaviorRouter); 
+      });
+  
+  module.exports = app;
+}
+else{
   console.log("in production")
   const dbName = 'touchpoint';
   const dbUriSecretName = "projects/903480499371/secrets/MONGO_DB_URI/versions/latest";
@@ -83,37 +117,3 @@ if (process.env.location === "production"){
 
   module.exports = app;
 }
-else{
-  console.log("in local development")
-  const jwtLoginSecret = process.env.JWT_LOGIN_SECRET;
-  console.log("jwtloginsecret",jwtLoginSecret)
-  const jwtVerifySecret = process.env.JWT_VERIFY_SECRET;
-  const emailObj = {
-      email: process.env.EMAIL_USERNAME,
-      password: process.env.EMAIL_PASSWORD
-  }
-  
-  app.use(function(req, res, next) {
-      req.jwtLoginSecret = jwtLoginSecret;
-      req.jwtVerifySecret = jwtVerifySecret;
-      req.emailCredentials = emailObj;
-      next();
-  });
-  
-  
-  mongo.connect(process.env.MONGO_DB_URI, async function(err) {
-          //Add routes here
-          if (err) {
-            console.log("throwing error inside mongo connect");
-            throw err;
-          }
-          app.use("/test", testRouter);
-          app.use("/api/auth", authRouter);
-          app.use("/api/period", classPeriodRouter);
-          app.use("/api/behavior", behaviorRouter); 
-      });
-  
-  module.exports = app;
-}
-
-
